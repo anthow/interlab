82272465d7ab46e176ecac2dd510cd5a6c4c2840
anthow <anthonyenglebert@gmail.com>
2023-03-13 10:41:50 +0100

    changement menu

--- src/components/navbar/interlab/index.js
+++ src/components/navbar/interlab/index.js
@@ -1,24 +1,40 @@
-import React from "react"
-import { Link } from "gatsby"
-
-
-
+import React from "react";
+import { Link } from "gatsby";
+import { useStaticQuery, graphql } from "gatsby";
 
 const NavbarInterlab = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      contentfulMenu {
+        rubrique1
+        rubrique2
+        rubrique3
+        rubrique4
+      }
+    }
+  `);
+  return (
+    <nav>
+      <ul className="flex text-orange-interra font-bold text-lg w-full  flex-col md:flex-row justify-center gap-5 underline md:gap-10 ">
+        <li className="whitespace-nowrap w-max">
+          {" "}
+          <Link to="/entrepreneurs">{data.contentfulMenu.rubrique1}</Link>
+        </li>
+        <li className=" whitespace-nowrap w-max">
+          {" "}
+          <Link to="/coachs">{data.contentfulMenu.rubrique2}</Link>
+        </li>
+        <li className="whitespace-nowrap w-max">
+          {" "}
+          <Link to="/experts">{data.contentfulMenu.rubrique3}</Link>
+        </li>
+        <li className="whitespace-nowrap w-max">
+          {" "}
+          <Link to="/partenaires">{data.contentfulMenu.rubrique4}</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
-    return (
-<nav>
-    <ul className="flex text-orange-interra font-bold text-lg w-full  flex-col md:flex-row justify-center gap-5 underline md:gap-10 ">
-        <li className="whitespace-nowrap w-max"> <Link to="/entrepreneurs">Nos entrepreneurs</Link></li>
-        <li className="whitespace-nowrap w-max" > <Link to="/experts">Nos Experts</Link></li>
-        <li className=" whitespace-nowrap w-max"> <Link to="/coachs">Nos coachs</Link></li>
-        <li className="whitespace-nowrap w-max"> <Link to="/partenaires">Nos partenaires</Link></li>
-
-    </ul>
-</nav>
-            
-
-    )
-}
-
-export default NavbarInterlab
+export default NavbarInterlab;
